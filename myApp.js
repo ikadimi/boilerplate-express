dd6703083bb50d27023553d834c92804b196b4c4
ikadimi <ilkadimi@outlook.com>
2022-06-19 17:11:58 +0200

    adding style

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static("public"))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
